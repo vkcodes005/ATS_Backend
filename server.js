64d70711bf37bdacfa3fed598864bc89f5c0d32e
vaishnavi Kashyap <vaishnavikashyap005@gmail.com>
2026-05-31 18:04:20 +0530

    Allow admin authorization CORS header

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function sendJson(res, status, data) {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type"
+    "Access-Control-Allow-Headers": "Content-Type, Authorization"
   });
   res.end(JSON.stringify(data));
 }
